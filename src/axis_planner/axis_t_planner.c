9d6e3eb0a878a1c1d29ffc267b011437ed8ec50e
liangliang_siasun@163.com <liangliang_siasun@163.com>
2020-08-31 16:30:54 +0800

    add pause handle
    add pause handle at slip stage for T planner

--- src/axis_planner/axis_t_planner.c
+++ src/axis_planner/axis_t_planner.c
@@ -21,12 +21,15 @@ axis_dec_handle_unit,
 axis_pause_handle_unit,
 };
 
+#define DEBUG_SLIP_PAUSE
+
 void axis_planner_test()
 {
 	AXIS_INFO axis_test;
 	long count = 0;
 	unsigned char stage_num;
-	double last_traj_pos = 0.0;
+	unsigned char pause_flag = 0;
+	char move_status = 0;
 
 	axis_test.max_vel = 5.0;
 	axis_test.max_acc = 1.0;
@@ -37,19 +40,58 @@ void axis_planner_test()
 	axis_init_handle_unit(&axis_test);
 
 	printf("t0:%d,t1:%d,t2:%d\n", axis_test.plan_timer[0],axis_test.plan_timer[1],axis_test.plan_timer[2]);
+
 	for (count = 1; count < axis_test.plan_timer[2]; count++)
 	{
 		if (count <= axis_test.plan_timer[0])
-			stage_num = (unsigned char)ACC_STAGE;
-		else if (count <= axis_test.plan_timer[1])
-			stage_num = (unsigned char)SLIP_STAGE;
-		else
-			stage_num = (unsigned char)DEC_STAGE;
+		{
+			if (pause_flag ==1)
+			{
+				;
+			}
+			else
+			{
+				stage_num = (unsigned char)ACC_STAGE;
+			}
+		}
 
-		axis_t_planner[stage_num](&axis_test,count);
+		if ((count > axis_test.plan_timer[0])
+		 && (count <= axis_test.plan_timer[1]))
+		{
+#ifdef DEBUG_SLIP_PAUSE
+			if (count == (axis_test.plan_timer[0] + 5))
+			{
+				pause_flag = 1;
+			}
+#endif
+			if (pause_flag == 1)
+			{
+				stage_num = (unsigned char)PAUSE_STAGE;
+				count = axis_test.plan_timer[1] + 1;
+			}
+			else
+			{
+				stage_num = (unsigned char)SLIP_STAGE;
+			}
+
+		}
+
+		if ((count > axis_test.plan_timer[1])
+		 && (count < axis_test.plan_timer[2]))
+		{
+			stage_num = (unsigned char)DEC_STAGE;
+		}
+
+		move_status = axis_t_planner[stage_num](&axis_test,count);
 
 		printf("count:%d,stage:%d,%f\n", count, stage_num, axis_test.traj_pos);
-		last_traj_pos = axis_test.traj_pos;
+
+		if (move_status == 1)
+		{
+			pause_flag = 0;
+			return;
+		}
+
 	}
 }
 
@@ -113,9 +155,9 @@ static char axis_slip_handle_unit(AXIS_INFO *p_axis_info, long count)
 {
 	double percent_delta = 0.0;
 
-    si = si + 1;
-    percent_delta = 1 - si/p_axis_info->plan_timer[1];
-    p_axis_info->traj_pos = p_axis_info->target_pos - p_axis_info->delta_pos*percent_delta;
+	si = si + 1;
+	percent_delta = 1 - si/p_axis_info->plan_timer[1];
+	p_axis_info->traj_pos = p_axis_info->target_pos - p_axis_info->delta_pos*percent_delta;
 
 	return 0;
 }
@@ -124,14 +166,29 @@ static char axis_dec_handle_unit(AXIS_INFO *p_axis_info, long count)
 {
 	double percent_delta = 0.0;
 
-    si = si + 1 - (double)(count - p_axis_info->plan_timer[1])/p_axis_info->plan_timer[0];
-    percent_delta = 1 - si/p_axis_info->plan_timer[1];
-    p_axis_info->traj_pos = p_axis_info->target_pos - p_axis_info->delta_pos*percent_delta;
+	si = si + 1 - (double)(count - p_axis_info->plan_timer[1])/p_axis_info->plan_timer[0];
+	percent_delta = 1 - si/p_axis_info->plan_timer[1];
+	p_axis_info->traj_pos = p_axis_info->target_pos - p_axis_info->delta_pos*percent_delta;
 
 	return 0;
+
 }
 
 static char axis_pause_handle_unit(AXIS_INFO *p_axis_info, long count)
 {
-	return 0;
+	double percent_delta = 0.0;
+
+	si = si + 1 - (double)(count - p_axis_info->plan_timer[1])/p_axis_info->plan_timer[0];
+	percent_delta = 1 - si/p_axis_info->plan_timer[1];
+	p_axis_info->traj_pos = p_axis_info->target_pos - p_axis_info->delta_pos*percent_delta;
+
+	printf("count:%d, percent_delta:%f\n", count, percent_delta);
+	if (count == p_axis_info->plan_timer[0])
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
